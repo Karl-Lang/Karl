f6c0af92351a025f9c925dfff52533f512621a88
Aiko <46852063+ItsJustAiko@users.noreply.github.com>
2022-10-09 19:36:58 +0200

    Add ToDo List to README.md

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 ## Ryoko, my homemade programming language
 
 ```Currently in working, sorry ! >~<```
+
+## Todo list
+
+- [x] Print Statement
+- [x] Variable
+- [ ] Function
+- [ ] If Statement
+- [ ] While Statement
+- [ ] For Statement
+- [ ] Array
+- [ ] Class
+- [ ] Module
+- [ ] Import
+- [ ] Export
+- [ ] Operator
+- [ ] Comment
--- src/main/java/fr/aiko/Ryoko/Main.java
+++ src/main/java/fr/aiko/Ryoko/Main.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Lexer lexer = new Lexer("int: a=1;\nprint(\"RAAAH ENFIN\");\nprint(ab);");
+        Lexer lexer = new Lexer("int: a=1;\nprint(\"RAAAH ENFIN\");\nprint(a);");
         ArrayList<Token> tokens = lexer.tokens;
 
         Parser parser = new Parser(tokens);
